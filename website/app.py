3f7941ed60e6d854b48f406f1c97bd148e434cc2
Dinesh Priyantha <Priyanthadinesh2002@gmail.com>
2025-03-30 22:18:14 +0530

    Update

--- website/app.py
+++ website/app.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
-def index():
-   return render_template("index.html")
+def home():
+    return render_template('index.html')
+
+@app.route('/predict')
+def predict():
+    item = request.args.get('item', '')
+    if item == "laptop":
+        return render_template('laptop.html')  # Render Laptop Prediction Form
+    return "Item not found", 404
+
+@app.route('/predict/laptop', methods=['POST', 'GET'])
+def predict_laptop():
+    if request.method == 'POST':
+        Ram= request.form['Ram']
+
+        
+       Weight = request.form['Weight'] 
+        # Process the data and return the prediction
+        
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
